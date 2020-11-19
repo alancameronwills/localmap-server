7e351c8c650f8909ef1be1a89beb4e643a7ebe69
Alan Cameron Wills <alan@cameronwills.org>
2020-11-19 23:59:13 +0000

    PicFix expt

--- PicFix/index.js
+++ PicFix/index.js
@@ -4,28 +4,49 @@
 // https://azure.github.io/azure-storage-node/BlobService.html#listBlobsSegmentedWithPrefix__anchor
 
 let azure = require('azure-storage');
-//let jimp = require('jimp');
-module.exports = function (context, req) {
+let fs = require("fs");
+let jimp = require('jimp');
+module.exports = async function (context, req) {
     let connectionString = process.env.AzureWebJobsStorage;
     let blobService = azure.createBlobService(connectionString);
     getSegments(context, blobService, (context, entries, isMoreToCome, prvs) => {
         let got = prvs || new Date().toLocaleTimeString() + "\n";
         let count = 0;
         try {
+            /*
         entries.forEach(e => {
             if (e.contentSettings.contentType.startsWith("image")) {
                 got += "\n" + e.name + "\t" + e.contentLength + "\t" + e.contentSettings.contentType ;
                 count++;
+                await processBlob(container, e.name);
             }
-        });
+        }); */
+        for(let i = 0; i<entries.length; i++) {
+            if (e.contentSettings.contentType.startsWith("image")) {
+                got += "\n" + e.name + "\t" + e.contentLength + "\t" + e.contentSettings.contentType ;
+                count++;
+                await processBlob(container, e.name);
+            }
+            if (i > 3) break;
+        }
         if (isMoreToCome) { got += "=== " + count + "\n"; }
         if (!isMoreToCome) {
             context.res.body = got + "\n" + count + "\n";
-            context.done();
+            //context.done();
         }
         return got;
         } catch (ex) {context.log(ex);}
     });
+}
+
+async function processBlob (container, blobName) {
+    await jimp.read("https://deep-map.azurewebsites.net/media/" + blobName)
+        .then(image => image.scaleToFit(800,800).getBufferAsync(Jimp.AUTO)
+        //.then(buffer => blobService.createBlockBlobFromStream(container, "s_media/"+blobName, stream, buffer.length)))
+        .then (buffer => {
+            let writeStream = blobService.createWriteStreamToBlockBlob(container, "s_media/" + blobName);
+            fs.createReadStream(buffer).pipe(writeStream);
+        }));
 }
 
 /// chomp(blobMetadataArray, isMoreToCome, previousChomped) -> chomped
