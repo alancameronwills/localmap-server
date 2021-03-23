c3e11c91f7d71603d769270c374b4b4d14799f5c
Alan Cameron Wills <alan@cameronwills.org>
2021-03-23 11:58:12 +0000

    google libraries places

--- map/index.js
+++ map/index.js
@@ -17,7 +17,7 @@ try {
 }
 async function getFile (filename, context) {
     var rawFN = filename == "google"
-    ? `https://maps.googleapis.com/maps/api/js?key=${process.env.Client_Google_Map_K}&callback=mapModuleLoaded`
+    ? `https://maps.googleapis.com/maps/api/js?key=${process.env.Client_Google_Map_K}&callback=mapModuleLoaded&libraries=places`
     : `https://www.bing.com/api/maps/mapcontrol?key=${process.env.Client_Map_K}&callback=mapModuleLoaded`;
     
     return new Promise (function (resolve, reject) {
