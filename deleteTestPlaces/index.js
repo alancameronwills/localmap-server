558b33ab4923ee245bd475e5c6bcd402fb54c4a0
Alan Cameron Wills <alan@cameronwills.org>
2021-04-05 22:54:04 +0100

    t

--- deleteTestPlaces/index.js
+++ deleteTestPlaces/index.js
@@ -2,9 +2,11 @@
 // https://docs.microsoft.com/en-us/azure/cosmos-db/table-storage-how-to-use-nodejs#work-with-groups-of-entities
 
 let azure = require('azure-storage');
+//let blobService = require('@azure/storage-blob');
 module.exports = function (context, req) {
     let connectionString = process.env.AzureWebJobsStorage;
     let tableService = azure.createTableService(connectionString);
+    let blobService = azure.createBlobService(connectionString);
 
     let query = new azure.TableQuery().where("PartitionKey eq ?", process.env.TestProjectId);
     tableService.queryEntities("places", query, null, function(error,result, response) {
@@ -16,6 +18,22 @@ module.exports = function (context, req) {
         }
         context.done();
     });
+
+    function deleteSet(result) {
+        let batch = new azure.TableBatch();
+        result.array.forEach(element => {
+            if (element.RowKey._ != "320501040707199024165") {
+                element.PartitionKey._ = process.env.TestProjectId;
+                batch.deleteEntity (element);
+                let media = JSON.parse(element.Media._);
+                media.forEach(item => {
+                    blobService.deleteBlobIfExists("deepmap", "media/" + item.id, (err, result) =>{
+                        
+                    });
+                })
+            }
+        });
+    }
 
     /*
     if (req.query.partitionKey && req.query.RowKey && req.headers["x-ms-client-principal-id"]) {
