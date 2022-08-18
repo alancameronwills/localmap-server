ff5b1310122a663612c95690f6458e30ebb5fe54
Alan Cameron Wills <alan@cameronwills.org>
2022-08-18 22:22:55 +0100

    fix

--- fetchproxy/index.js
+++ fetchproxy/index.js
@@ -2,13 +2,17 @@
     const fetch = require("node-fetch");  
     module.exports = async function (context, req) {
         await fetch (req.query.url)
-        .then(data => data.blob()
-        .then (blob =>
+        .then(data => {
+            let h = data.headers;
+            data.blob().then (blob =>
             {  
                 context.log("1");
+                context.res.isRaw = true;
+                context.res.body = blob;
+                context.res.headers.set("content-type","image/jpeg");
                 context.res.send(blob);
-            }
-        ))
+            });
+        })
         .catch (error => {context.res = {status:400}; context.log("X "+error);})
     };
     
