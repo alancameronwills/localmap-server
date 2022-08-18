09fd19b1679591af3e6f2febd3edb05fd052d239
Alan Cameron Wills <alan@cameronwills.org>
2022-08-18 20:31:36 +0100

    probes

--- fetchproxy/index.js
+++ fetchproxy/index.js
@@ -2,10 +2,12 @@
     const fetch = require("node-fetch");  
     module.exports = async function (context, req) {
         await fetch (req.query.url)
-        .then((data) => data.text())
+        .then((data) => {context.log("0"); return data.text();})
         .then (data =>
             {  
+                context.log("1");
              parseString(data, (err, result) => {
+                context.log("2");
                  context.res = {
                     //status: 200, /* Defaults to 200 */
                     body: data
@@ -13,6 +15,6 @@
              });
             }
         )
-        .catch (error => context.res = {status:400})
+        .catch (error => {context.res = {status:400}; context.log("X "+error);})
     };
     
